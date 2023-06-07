eef7d8cfd063553034ef0ad045e4faae181bd803
PubwinSoft <129506547+PubwinSoft@users.noreply.github.com>
2023-06-07 13:20:31 +0800

    ChatNav.vue

--- frontend/src/components/ChatNav/ChatNav.vue
+++ frontend/src/components/ChatNav/ChatNav.vue
@@ -36,8 +36,8 @@ const navType = {
 const navConfigs = [
   {
     key: navType.github,
-    label: '开源地址',
-    url: 'https://github.com/adams549659584/go-proxy-bingai',
+    label: '帮助中心',
+    url: 'https://www.77388.cn/newbing',
   },
   {
     key: navType.version,
