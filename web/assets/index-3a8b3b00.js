7dc4c267cd3fabe0517c59039984cdb06ac4e450
PubwinSoft <129506547+PubwinSoft@users.noreply.github.com>
2023-06-08 15:52:10 +0800

    index-3a8b3b00.js

--- web/assets/index-3a8b3b00.js
+++ web/assets/index-3a8b3b00.js
@@ -1204,7 +1204,7 @@ import{r as O,w as Te,o as et,a as nt,i as Na,c as D,b as Ha,h as ja,d as pn,e a
  right: 0;
  top: 0;
  bottom: 0;
- width: 80px;
+ width: 300px;
  display: flex;
  align-items: center;
  transition: opacity .2s var(--n-bezier);
